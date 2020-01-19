1b7df5503f75c0d08260437d79319651cfcfad07
Suphala Patwardhan <suphala124@gmail.com>
2020-01-19 14:34:34 +0100

    Add files via upload

--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace BerlinClock
 {
@@ -9,61 +7,73 @@ namespace BerlinClock
     {
         public string convertTime(string aTime)
         {
-            string time = string.Empty;
-            string[] arr = aTime.Split(':');            
+
+            #region splitting the string to get hours, minutes and seconds separately
+            string[] arr = aTime.Split(':');
+            if (arr.Length != 3)
+            {
+                Console.WriteLine("String entered is not in correct format");
+            }
             int[] timeArray = new int[3];
             int count = 0;
             foreach (string st in arr)
-                timeArray[count++] = Convert.ToInt32(st);
+            {
+                try
+                {
+                    timeArray[count++] = Convert.ToInt32(st);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The string value '{1}' is not in a recognizable format.", st);
+                }
+            }
+            if (timeArray[0] < 0 || timeArray[0] > 24 || timeArray[1] < 0 || timeArray[1] > 59 || timeArray[2] < 0 || timeArray[2] > 59)
+            {
+                Console.WriteLine("Please enter time in correct format.");
+            }
+            #endregion
 
             string[] myTime = new string[5];
 
+            #region determining the value for Row 1
             myTime[0] = timeArray[2] % 2 == 0 ? "Y" : "O";
+            #endregion
+
+            #region determining the value for Row 2
             int tempHrs = timeArray[0] / 5;
-            switch (tempHrs)
-            {
-                case 0: myTime[1] = "OOOO"; break;
-                case 1: myTime[1] = "ROOO"; break;
-                case 2: myTime[1] = "RROO"; break;
-                case 3: myTime[1] = "RRRO"; break;
-                case 4: myTime[1] = "RRRR"; break;
-                default:
-                    break;
-            }
+            myTime[1] = "OOOO";
+            var regex = new Regex(Regex.Escape("O"));
+            myTime[1] = regex.Replace(myTime[1], "R", tempHrs);
+            #endregion
+
+            #region determining the value for Row 3
             tempHrs = timeArray[0] % 5;
-            switch (tempHrs)
-            {
-                case 0: myTime[2] = "OOOO"; break;
-                case 1: myTime[2] = "ROOO"; break;
-                case 2: myTime[2] = "RROO"; break;
-                case 3: myTime[2] = "RRRO"; break;
-                case 4: myTime[2] = "RRRR"; break;
-                default:
-                    break;
-            }
+            myTime[2] = "OOOO";
+            regex = new Regex(Regex.Escape("O"));
+            myTime[2] = regex.Replace(myTime[2], "R", tempHrs);
+            #endregion
+
+            #region determining the value for Row 4
             int tempmin = timeArray[1] / 5;
             myTime[3] = "";
             for (count = 1; count <= tempmin; count++)
-            {
                 myTime[3] = count % 3 == 0 ? myTime[3] + "R" : myTime[3] + "Y";
-            }
+            
             while (tempmin < 11)
             {
                 myTime[3] += "O";
                 tempmin++;
-            }            
-            int tempSecMod = timeArray[2] % 5;
-            switch (tempSecMod)
-            {
-                case 0: myTime[4] = "OOOO"; break;
-                case 1: myTime[4] = "YOOO"; break;
-                case 2: myTime[4] = "YYOO"; break;
-                case 3: myTime[4] = "YYYO"; break;
-                case 4: myTime[4] = "YYYY"; break;
-                default:
-                    break;
             }
-            time = System.String.Join("\r\n", myTime);
+            #endregion
+
+            #region determining the value for Row 5
+            int tempSecMod = timeArray[2] % 5;
+            myTime[4] = "OOOO";
+            regex = new Regex(Regex.Escape("O"));
+            myTime[4] = regex.Replace(myTime[4], "Y", tempSecMod);
+            #endregion
+
+            string time = string.Join("\r\n", myTime);
             return time;
         }
     }
